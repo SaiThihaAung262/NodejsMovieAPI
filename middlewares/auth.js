399960ffd71231386d5891d75651d4d93846dd77
minghua <minghua@gmail.com>
2022-06-29 15:16:43 +0630

    moidfy:

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,18 @@ const { readFileSync } = require("fs");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+    console.log(req.get("Authorization"));
     if (!req.get("Authorization")) {
         return next(createError(401));
     }
+
     const token = req.get("Authorization").split(" ")[1]; //req token
+
     const secret = readFileSync("./private.key"); // token that we sent
 
+    const decode = jwt.verify(token, secret); // decode jwt token
+
     try {
-        const decode = jwt.verify(token, secret); // verify token
         req.user = { _id: decode._id, username: decode.username };
         next();
     } catch (error) {
