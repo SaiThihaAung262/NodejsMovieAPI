ded9bd2838a32e8d9a06e641041cf832a711bee0
minghua <minghua@gmail.com>
2022-07-01 17:45:17 +0630

    Modify

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer(app);
 server.listen(process.env.PORT);
 
 server.on("listening", () => {
-    console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
