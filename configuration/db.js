a288f8e3f456b3514fb6225a3bd4d604ae1a15f6
minghua <minghua@gmail.com>
2022-07-01 10:03:10 +0630

    moidfy

--- configuration/db.js
+++ configuration/db.js
@@ -5,7 +5,10 @@ const dbConneciton = (coll, cb) => {
     MongoClient.connect(_uri)
         .then(async(client) => {
             const db = client.db("sample_mflix").collection(coll);
+            // const db2 = client.db("sample_mflix").collection(coll2);
+            // await cb(db, db2);
             await cb(db);
+
             client.close();
         })
         .catch((err) => {
--- controller/commentController.js
+++ controller/commentController.js
@@ -3,87 +3,87 @@ const createError = require("http-errors");
 const { Comment } = require("../models");
 
 const postComment = (req, res, next) => {
-    /**
-     *Parameters for created Comments
-     * _id
-     * userId>req.user._id
-     * username>req.user.username
-     * movieId>req.body.movieId
-     * text>req.body.text
-     */
+  /**
+   *Parameters for created Comments
+   * _id
+   * userId>req.user._id
+   * username>req.user.username
+   * movieId>req.body.movieId
+   * text>req.body.text
+   */
 
-    if (!ObjectID.isValid(req.body.movieId)) {
-        return next(createError(400));
-    }
+  if (!ObjectID.isValid(req.body.movieId)) {
+    return next(createError(400));
+  }
 
-    const error = Comment.validate(req.body["text"]);
+  const error = Comment.validate(req.body["text"]);
 
-    if (error) {
-        return next(error);
-    }
+  if (error) {
+    return next(error);
+  }
 
-    const commentData = {
-        text: req.body["text"],
-    };
-    commentData.userId = new ObjectID(req.user["_id"]);
-    commentData.username = req.user["username"];
-    commentData.movieId = new ObjectID(req.body["movieId"]);
+  const commentData = {
+    text: req.body["text"],
+  };
+  commentData.userId = new ObjectID(req.user["_id"]);
+  commentData.username = req.user["username"];
+  commentData.movieId = new ObjectID(req.body["movieId"]);
 
-    const comment = new Comment(commentData);
-    comment
-        .save()
-        .then(() => {
-            res.status(201).json({
-                err_message: "Comment created successfully",
-            });
-        })
-        .catch((err) => {
-            next(createError(500));
-        });
+  const comment = new Comment(commentData);
+  comment
+    .save()
+    .then(() => {
+      res.status(201).json({
+        err_message: "Comment created successfully",
+      });
+    })
+    .catch((err) => {
+      next(createError(500));
+    });
 };
 
 const putComment = (req, res, next) => {
-    if (!ObjectID.isValid(req.body.commentId)) {
-        return next(createError(400));
-    }
+  if (!ObjectID.isValid(req.body.commentId)) {
+    return next(createError(400));
+  }
 
-    const error = Comment.validate(req.body["text"]);
+  const error = Comment.validate(req.body["text"]);
 
-    if (error) {
-        return next(error);
-    }
+  if (error) {
+    return next(error);
+  }
 
-    const commentId = new ObjectID(req.body.commentId);
-    Comment.edit(commentId, req.body.text)
-        .then(() => {
-            res.status(201).json({
-                err_message: "Update comment successfully",
-            });
-        })
-        .catch((error) => {
-            next(createError(500));
-        });
+  const commentId = new ObjectID(req.body.commentId);
+  Comment.edit(commentId, req.body.text)
+    .then(() => {
+      res.status(201).json({
+        err_message: "Update comment successfully",
+      });
+    })
+    .catch((error) => {
+      next(createError(500));
+    });
 };
 
 const deleteComment = (req, res, next) => {
-    if (!ObjectID.isValid(req.body.commentId)) {
-        return next(createError(400));
-    }
-    const commentId = new ObjectID(req.body.commentId);
+  if (!ObjectID.isValid(req.body.commentId)) {
+    return next(createError(400));
+  }
+  const commentId = new ObjectID(req.body.commentId);
 
-    Comment.delete(commentId)
-        .then(() => {
-            res.status(201).json({
-                err_message: "Delete comment successfully",
-            });
-        })
-        .catch((error) => {
-            next(createError(500));
-        });
+  Comment.delete(commentId)
+    .then(() => {
+      res.status(201).json({
+        err_message: "Delete comment successfully",
+      });
+    })
+    .catch((error) => {
+      next(createError(500));
+    });
 };
 
 module.exports = {
-    postComment,
-    putComment,
-    deleteComment,
+  postComment,
+  putComment,
+  deleteComment,
 };
